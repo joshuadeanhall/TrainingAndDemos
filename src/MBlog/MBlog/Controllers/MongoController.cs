090ed2abfde292bb1f521110998cc13c370a3970
joshuadeanhall@gmail.com <joshuadeanhall@gmail.com>
2013-01-26 12:33:57 -0500

    Setup logging facility

--- src/MBlog/MBlog/Controllers/MongoController.cs
+++ src/MBlog/MBlog/Controllers/MongoController.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using Castle.Core.Logging;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,7 @@ namespace MBlog.Controllers
 {
     public abstract class MongoController : Controller
     {
+        public ILogger Logger { get; set; }
         public MongoDatabase Database { get; set; }
 
     }
