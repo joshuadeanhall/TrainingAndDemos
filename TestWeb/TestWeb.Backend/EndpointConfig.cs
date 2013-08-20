a20410cfc95eff1033d39f46cf7c9565f064d5aa
Joshua Hall <joshuadeanhall@gmail.com>
2013-08-20 15:47:35 -0400

    Backend Solution Added

--- TestWeb/TestWeb.Backend/EndpointConfig.cs
+++ TestWeb/TestWeb.Backend/EndpointConfig.cs
@@ -9,7 +9,7 @@ namespace TestWeb.Backend
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, UsingTransport<Msmq>
+	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server, UsingTransport<RabbitMQ>
 	{
     }
 }
