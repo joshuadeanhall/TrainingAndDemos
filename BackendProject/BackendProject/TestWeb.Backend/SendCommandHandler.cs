7954aa2f0ee5d4b19bf0c1ddc0840f1e5bd6372d
unknown <jdhall@development.home.com>
2013-08-20 20:52:46 -0400

    Added mongo just to store some stuff for testing.

--- BackendProject/BackendProject/TestWeb.Backend/SendCommandHandler.cs
+++ BackendProject/BackendProject/TestWeb.Backend/SendCommandHandler.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using NServiceBus;
 using NServiceBus.Logging;
 using TestWeb.Messages;
+using System.Configuration;
+using MongoDB.Driver;
 
 namespace TestWeb.Backend
 {
@@ -15,6 +17,19 @@ namespace TestWeb.Backend
         public IBus Bus { get; set; }
         public void Handle(SendCommandMessage message)
         {
+
+            var connectionString = ConfigurationManager.AppSettings.Get("MongoDatabaseUrl") ??
+                                   ConfigurationManager.AppSettings.Get("MONGOHQ_URL") ??
+                                   ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            var url = new MongoUrl(connectionString);
+            var client = new MongoClient(url);
+            var server = client.GetServer();
+
+            var database = server.GetDatabase(url.DatabaseName);
+
+            var collection = database.GetCollection<SampleData>("example");
+            var example = new SampleData { Name = message.Name };
+            collection.Insert(example);          
             Logger.Info("Starting message");
             Bus.Send(new SendCompleteMessage {Name = string.Format("Message {0} processed name is {1}", message.Id, message.Name)});
         }
