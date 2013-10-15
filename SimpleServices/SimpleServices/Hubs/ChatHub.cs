41e2ba0c2145096eea047cc4e8e95cd21831e226
Joshua Hall <joshuadeanhall@gmail.com>
2013-10-14 23:16:39 -0400

    Initial check in.  Currently using console apps.  Will switch this to windows services later.

---
+++ SimpleServices/SimpleServices/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace SimpleServices.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string username, string message)
+        {
+            Clients.Others.MessageSent(string.Format("{0} sent message {1}", username, message));
+        }
+    }
+}
---
+++ SimpleServices/SimpleServices/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace SimpleServices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8050/"))
+            {
+                Console.WriteLine("Server running at http://localhost:8050/");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                map.UseCors(CorsOptions.AllowAll);
+
+                var hubConfig = new HubConfiguration
+                {
+                    EnableDetailedErrors = true,
+                    EnableJSONP = true
+                };
+
+                map.RunSignalR(hubConfig);
+            });
+        }
+    }
+}
