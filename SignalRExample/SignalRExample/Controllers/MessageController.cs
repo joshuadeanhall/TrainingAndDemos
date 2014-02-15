428f6b0593684db2e1ad59a218ab37867f2567b0
Joshua Hall <joshuadeanhall@gmail.com>
2014-02-15 15:54:04 -0500

    Testing

---
+++ SignalRExample/SignalRExample/Controllers/MessageController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRExample.Controllers
+{
+    public class MessageController : Controller
+    {
+        //
+        // GET: /Message/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Message/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Message/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Message/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
