77e004f1894a6bea83aa73df3c5c4b69f41730b7
Joshua Hall <joshuadeanhall@gmail.com>
2014-02-15 21:59:21 -0500

    Added simple topshelf service

---
+++ SignalRExample/MessageService/PostDeploy.ps1
@@ -0,0 +1,7 @@
+﻿sc.exe delete MyService-service1 | Write-Host
+
+Start-Sleep -Second 3
+
+# install is done via TopShelf's command line to install the proper type of server.
+
+& "MessageService.exe" "install" | Write-Host
