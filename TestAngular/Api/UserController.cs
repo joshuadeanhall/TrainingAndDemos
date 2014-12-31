745713936f7d68b0a7f3d3d4d95862ebd2c5a68b
Josh Hall <joshuadeanhall@gmail.com>
2014-12-31 13:40:16 -0500

    Initial

---
+++ TestAngular/Api/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestAngular.Api
+{
+    public class UserController : ApiController
+    {
+        public HttpStatusCode Post(UserModel user)
+        {
+            throw new Exception("Test Exception");
+            return HttpStatusCode.Accepted;
+        }
+
+        public HttpStatusCode Put(UserModel user)
+        {
+            return HttpStatusCode.Accepted;
+        }
+    }
+
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ TestAngular/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestAngular.Models;
+
+namespace TestAngular.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var reportType = 5;
+            ReportType enumReportType = (ReportType) reportType;
+            GetValue(enumReportType);
+            if (enumReportType == null)
+                throw new Exception();
+            return View();
+        }
+
+        private string GetValue(ReportType enumReportType)
+        {
+            string x = enumReportType.ToString();
+
+            var stringValueAttributeArray = enumReportType.GetType().GetField(enumReportType.ToString()).GetCustomAttributes(typeof(StringValueAttribute), false) as StringValueAttribute[];
+            if (stringValueAttributeArray.Length <= 0)
+                return (string)null;
+            return stringValueAttributeArray[0].Value;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    internal class StringValueAttribute : StringValueBaseAttribute
+    {
+    }
+
+    internal class StringValueBaseAttribute
+    {
+        public virtual string Value { get; protected set; }
+    }
+}
